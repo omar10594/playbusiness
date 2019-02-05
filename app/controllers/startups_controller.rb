88916bb6180505cf744560c12812cd542a7f207d
Guillermo Omar Lopez Lopez <omar@nutriconsultor.io>
2019-02-04 21:31:57 -0800

    feat: added basic CRUD of Startup model.

---
+++ app/controllers/startups_controller.rb
@@ -0,0 +1,49 @@
+class StartupsController < ApplicationController
+    def index
+        @startups = Startup.all
+    end
+
+    def show
+        @startup = Startup.find(params[:id]);
+    end
+
+    def new
+        @startup = Startup.new
+    end
+
+    def edit
+        @startup = Startup.find(params[:id])
+    end
+
+    def create
+        @startup = Startup.new(startup_params)
+
+        if @startup.save
+            redirect_to @startup
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @startup = Startup.find(params[:id])
+       
+        if @startup.update(startup_params)
+          redirect_to @startup
+        else
+          render 'edit'
+        end
+    end
+
+    def destroy
+        @startup = Startup.find(params[:id])
+        @startup.destroy
+
+        redirect_to startups_path
+    end
+
+    private
+        def startup_params
+            params.require(:startup).permit(:name, :description)
+        end
+end
---
+++ app/models/startup.rb
@@ -0,0 +1,4 @@
+class Startup < ApplicationRecord
+    validates :name,    presence: true,
+                        length: { minimum: 5 }
+end
